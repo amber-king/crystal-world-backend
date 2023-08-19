3ff750035fd6f8cbf3f43e88def0ff02dc0eebee
amber-king <amberking@pursuit.org>
2023-08-19 16:23:03 -0400

    mounted controller to app.js & typed in clarifying code notes as needed

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
+// Dependicies
 const express = require("express");
+const crystalsControllers = require("./controllers/crystalsControllers");
 
+// Configuration
 const app = express();
 
+// Middleware
 app.use(express.json());
 
+// Routes 
 app.get("/", (req, res) => {
   res.json("Crystal World 💎");
 });
+
+// mount of controller to keep track of crystal index page changes
+app.use("/crystals", crystalsControllers)
+
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,8 +1,11 @@
+// Dependencies
 const app = require("./app.js");
 
+// Configuration
 require("dotenv").config();
 const PORT = process.env.PORT;
 
+// Listen
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
