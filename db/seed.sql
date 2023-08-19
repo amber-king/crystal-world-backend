44c50bcd358eb09d6d49b91de798362605305b58
amber-king <amberking@pursuit.org>
2023-08-19 15:50:20 -0400

    luster options & hardness scale insertaions made

---
+++ db/seed.sql
@@ -0,0 +1,25 @@
+\ c crystal_world
+INSERT INTO luster_options (option_name)
+VALUES
+  ('Vitreous'),
+  ('Pearly'),
+  ('Metallic'),
+  ('Dull'),
+  ('Adamantine'),
+  ('Greasy'),
+  ('Waxy'),
+  ('Silky'),
+  ('Resinous');
+
+  INSERT INTO hardness_options (rating)
+VALUES
+  (1),
+  (2),
+  (3),
+  (4),
+  (5),
+  (6),
+  (7),
+  (8),
+  (9),
+  (10);
