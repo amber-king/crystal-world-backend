31a71207bcf231e3029a5bdfa308377c3ebe9fb8
amber-king <amberking@pursuit.org>
2023-08-19 20:34:30 -0400

    cors package added on

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 // Dependicies
 const express = require("express");
+const cors - require("cors")
 const crystalsControllers = require("./controllers/crystalsControllers");
 
 // Configuration
 const app = express();
 
 // Middleware
+app.use(cors())
 app.use(express.json());
+
 
 // Routes 
 app.get("/", (req, res) => {
