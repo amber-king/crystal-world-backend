677d9b6a769c8fbf96011c1d05319ad19ac1ab87
amber-king <amberking@pursuit.org>
2023-08-19 20:42:52 -0400

    edit for cors error

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // Dependicies
 const express = require("express");
-const cors - require("cors")
+const cors = require("cors")
 const crystalsControllers = require("./controllers/crystalsControllers");
 
 // Configuration
