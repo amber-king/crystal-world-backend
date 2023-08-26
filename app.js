d2263c68c484032e226311aeb26ea02fd008a6ca
amber-king <amberking@pursuit.org>
2023-08-26 14:28:48 -0400

    working w/ no luster & hardness routes

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // Dependicies
 const express = require("express");
 const cors = require("cors");
-const db = require("./db")
+const db = require("./db");
 const crystalsControllers = require("./controllers/crystalsControllers");
 
 // Configuration
@@ -17,15 +17,28 @@ app.get("/", (req, res) => {
   res.json("Crystal World 💎");
 });
 
-app.get("/luster_options", async (req, res) => {
-  try {
-    const lusterOptions = await db.any("SELECT * FROM luster_options");
-    res.json({ lusterOptions });
-  } catch (error) {
-    console.error("Error fetching luster options:", error);
-    res.status(500).json({ error: "Error fetching luster options" });
-  }
-});
+// app.get("/luster_options", async (req, res) => {
+//   console.log("Received request for luster options");
+//   try {
+//     const lusterOptions = await db.any(
+//       "SELECT DISTINCT luster_name FROM crystals"
+//     );
+//     res.json({ lusterOptions });
+//   } catch (error) {
+//     console.error("Error fetching luster options:", error);
+//     res.status(500).json({ error: "Error fetching luster options" });
+//   }
+// });
+
+// app.get("/hardness_options", async (req, res) => {
+//   try {
+//     const hardnessOptions = await db.any("SELECT rating FROM hardness_options");
+//     res.json({ hardnessOptions });
+//   } catch (error) {
+//     console.error("Error fetching hardness options:", error);
+//     res.status(500).json({ error: "Error fetching hardness options" });
+//   }
+// });
 
 // Crystals Routes - mount of controller to keep track of crystal index page changes
 app.use("/crystals", crystalsControllers);
